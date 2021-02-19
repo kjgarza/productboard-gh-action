1343fe2084dfe8268e135ad86c07b3860d66eb4e
kjgarza <kgarza@datacite.org>
2021-02-19 07:37:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ async function run() {
     const octokit = github.context;
     const {issue} = octokit.payload;
 
-    console.log('issue : ffff ', issue.issue.user.login);
+    console.log('issue : ffff ', issue.issue);
 
     const PayloadSchema = {
 //       issueCreatedBy: issue.user.login,
